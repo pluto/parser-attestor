2867df59f163983dc4e8e43dde7f7f5c02e266bc
Colin Roberts <colin@autoparallel.xyz>
2024-07-24 12:29:33 -0600

    feat: basic parse for

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,16 @@
-const EXAMPLE_JSON: &[u8] = include_bytes!("../example.json");
+pub const EXAMPLE_JSON: &[u8] = include_bytes!("../example.json");
+pub const VENMO_JSON: &[u8] = include_bytes!("../venmo_response.json");
 
 pub struct Machine<const MATCH_LENGTH: usize> {
     pub key_byte_match: [u8; MATCH_LENGTH],
-    pub target_byte_start_index: Option<u8>,
-    pub target_byte_end_index: Option<u8>,
 }
 
 impl<const MATCH_LENGTH: usize> Machine<MATCH_LENGTH> {
     pub fn new(key_byte_match: [u8; MATCH_LENGTH]) -> Self {
-        Machine {
-            key_byte_match,
-            target_byte_start_index: None,
-            target_byte_end_index: None,
-        }
+        Machine { key_byte_match }
     }
 
-    pub fn extract(&self, data_bytes: &'static [u8]) -> Option<&[u8]> {
+    pub fn extract<'a>(&self, data_bytes: &'a [u8]) -> Option<&'a [u8]> {
         assert!(data_bytes.len() > MATCH_LENGTH);
         let mut flag = false;
         'outer: for i in 0..(data_bytes.len() - MATCH_LENGTH) {
@@ -26,12 +21,17 @@ impl<const MATCH_LENGTH: usize> Machine<MATCH_LENGTH> {
                 flag = true;
             }
             if flag {
-                return Some(&data_bytes[i..i + MATCH_LENGTH]);
+                let start_index = i + MATCH_LENGTH + 1;
+                let mut value_length = 0;
+                while data_bytes[start_index + value_length] != b"}"[0] {
+                    value_length += 1;
+                }
+                return Some(&data_bytes[start_index..start_index + value_length]);
             } else {
                 return None;
             }
         }
-        None
+        unreachable!()
     }
 }
 
@@ -41,10 +41,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn get_title() {
-        let key_byte_match = *b"\"title\"";
+    fn get_value() {
+        let key_byte_match = *b"\"value\"";
         let machine = Machine::new(key_byte_match);
-        let title = String::from_utf8_lossy(machine.extract(EXAMPLE_JSON).unwrap());
-        println!("{title:?}");
+        let value = String::from_utf8_lossy(machine.extract(VENMO_JSON).unwrap());
+        println!("{value:?}");
     }
 }
