a32c6890e0d9753e5ee323002fcacd701e44ef84
Colin Roberts <colin@autoparallel.xyz>
2024-08-06 13:10:37 -0600

    feat: `Switch` outputs arrays

--- circuits/parser.circom
+++ circuits/parser.circom
@@ -39,8 +39,11 @@ template Parser() {
     // - ASCII char: ` `
     var space = 32;
 
-    // TODO: Rewrite switch to also take the switch cases instead of enforcing 0,1,...n,
-    component matcher = Switch(8); // 7 possible characters above, 1 case for non-chars above
+    // Outputs
+
+    // component matcher = Switch(7, 4); // 7 possible characters above
+    // matcher.branches <== [target_byte, start_brace, end_brace, start_bracket, end_bracket, quote];
+    // matches.vals <== 
     
 }
 
@@ -48,34 +51,41 @@ template Parser() {
 This function is creates an exhaustive switch statement from `0` up to `n`.
 
 # Inputs:
-- `n`: the number of switch cases
+- `m`: the number of switch cases
+- `n`: the output array length
 - `case`: which case of the switch to select
-- `vals[n]`: the value that is emitted for a given switch case (e.g., `val[i]` on `case == i`)
-- `out`: the selected output value
+- `branches[m]`: the values that enable taking different branches in the switch 
+    (e.g., if `branch[i] == 10` then if `case == 10` we set `out == `vals[i]`)
+- `vals[m][n]`: the value that is emitted for a given switch case 
+    (e.g., `val[i]` array is emitted on `case == `branch[i]`)
+- `out[n]`: the selected output value
 
 # Constraints:
 - `case`: must be in the range `0, 1, ..., n-1`
 */
-template Switch(n) {
+template Switch(m, n) {
+    assert(m > 0);
     assert(n > 0);
     signal input case;
-    signal input branches[n];
-    signal input vals[n];
-    signal output out;
+    signal input branches[m];
+    signal input vals[m][n];
+    signal output out[n];
 
     // Verify that the `case` is in the possible set of matches (0..n exlusive)
-    var match_array[n];
-    component indicator[n];
-    signal component_out[n];
-    var sum = 0;
-    for(var i = 0; i < n; i++) {
+    var match_array[m];
+    component indicator[m];
+    signal component_out[m][n];
+    var sum[n];
+    for(var i = 0; i < m; i++) {
         match_array[i] = case - branches[i];
         indicator[i] = IsZero();
         indicator[i].in <== case - branches[i]; 
-        component_out[i] <== indicator[i].out * vals[i];
-        sum += component_out[i];
+        for(var j = 0; j < n; j++) {
+            component_out[i][j] <== indicator[i].out * vals[i][j];
+            sum[j] += component_out[i][j];
+        }
     }
-    component matchChecker = Contains(n);
+    component matchChecker = Contains(m);
     matchChecker.in <== 0;
     matchChecker.array <== match_array;
     matchChecker.out === 1;
--- circuits/test/parser.test.ts
+++ circuits/test/parser.test.ts
@@ -8,50 +8,50 @@ describe("parser", () => {
             circuit = await circomkit.WitnessTester(`Switch`, {
                 file: "circuits/parser",
                 template: "Switch",
-                params: [3],
+                params: [3, 2],
             });
             console.log("#constraints:", await circuit.getConstraintCount());
         });
 
-        it("(valid) witness: case = 0, branches = [0, 1, 2], vals = [69, 420, 1337]", async () => {
+        it("(valid) witness: case = 0, branches = [0, 1, 2], vals = [[69,0], [420,1], [1337,2]]", async () => {
             await circuit.expectPass(
-                { case: 0, branches: [0, 1, 2], vals: [69, 420, 1337] },
-                { out: 69 },
+                { case: 0, branches: [0, 1, 2], vals: [[69, 0], [420, 1], [1337, 2]] },
+                { out: [69, 0] },
             );
         });
 
-        it("(valid) witness: case = 1, branches = [0, 1, 2], vals = [69, 420, 1337]", async () => {
+        it("(valid) witness: case = 1, branches = [0, 1, 2], vals = [[69,0], [420,1], [1337,2]]", async () => {
             await circuit.expectPass(
-                { case: 1, branches: [0, 1, 2], vals: [69, 420, 1337] },
-                { out: 420 },
+                { case: 1, branches: [0, 1, 2], vals: [[69, 0], [420, 1], [1337, 2]] },
+                { out: [420, 1] },
             );
         });
 
-        it("(valid) witness: case = 2, branches = [0, 1, 2], vals = [69, 420, 1337]", async () => {
+        it("(valid) witness: case = 2, branches = [0, 1, 2], vals = [[69,0], [420,1], [1337,2]]", async () => {
             await circuit.expectPass(
-                { case: 2, branches: [0, 1, 2], vals: [69, 420, 1337] },
-                { out: 1337 },
+                { case: 2, branches: [0, 1, 2], vals: [[69, 0], [420, 1], [1337, 2]] },
+                { out: [1337, 2] },
             );
         });
 
-        it("(invalid) witness: case = 3, branches = [0, 1, 2], vals = [69, 420, 1337]", async () => {
+        it("(invalid) witness: case = 3, branches = [0, 1, 2], vals = [[69,0], [420,1], [1337,2]]", async () => {
             await circuit.expectFail(
-                { case: 3, branches: [0, 1, 2], vals: [69, 420, 1337] },
+                { case: 3, branches: [0, 1, 2], vals: [[69, 0], [420, 1], [1337, 2]] },
             );
         });
 
-        it("(valid) witness: case = 420, branches = [69, 420, 1337], vals = [10, 20, 30]", async () => {
+        it("(valid) witness: case = 420, branches = [69, 420, 1337], vals = [[10,3], [20,5], [30,7]]", async () => {
             await circuit.expectPass(
-                { case: 420, branches: [69, 420, 1337], vals: [10, 20, 30] },
+                { case: 420, branches: [69, 420, 1337], vals: [[10, 3], [20, 5], [30, 7]] },
+                { out: [20, 5] }
             );
         });
 
-        it("(invalid) witness: case = 0, branches = [69, 420, 1337], vals = [10, 20, 30]", async () => {
+        it("(invalid) witness: case = 0, branches = [69, 420, 1337], vals = [[10,3], [20,5], [30,7]]", async () => {
             await circuit.expectFail(
-                { case: 0, branches: [69, 420, 1337], vals: [10, 20, 30] },
+                { case: 0, branches: [69, 420, 1337], vals: [[10, 3], [20, 5], [30, 7]] },
             );
         });
-
 
     });
 
