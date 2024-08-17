cbb657cf9f2f03d001e172164ee234bcfe1e9e1b
lonerapier <lonerapier@proton.me>
2024-08-17 18:23:04 +0530

    add tests for string extractor

---
+++ circuits/test/fetcher.test.ts
@@ -0,0 +1,73 @@
+import { circomkit, WitnessTester, generateDescription } from "./common";
+import { readFileSync } from "fs";
+import { join } from "path";
+
+describe("ExtractValue", () => {
+    let circuit: WitnessTester<["data", "key"], ["value"]>;
+
+    function readInputFile(filename: string, key: string): [number[], number[], number[]] {
+        const value_string_path = join(__dirname, "..", "..", "json_examples", "test", filename);
+
+        let input: number[] = [];
+        let output: number[] = [];
+
+        let data = readFileSync(value_string_path, 'utf-8');
+
+        let keyUnicode: number[] = [];
+        for (let i = 0; i < key.length; i++) {
+            keyUnicode.push(key.charCodeAt(i));
+        }
+
+        const byteArray = [];
+        for (let i = 0; i < data.length; i++) {
+            byteArray.push(data.charCodeAt(i));
+        }
+        input = byteArray;
+
+        let jsonFile = JSON.parse(data);
+        let value: string = jsonFile[key];
+        for (let i = 0; i < value.length; i++) {
+            output.push(value.charCodeAt(i));
+        }
+
+        return [input, keyUnicode, output];
+    }
+
+    it("value_string: {\"a\": \"b\"}", async () => {
+        circuit = await circomkit.WitnessTester(`Extract`, {
+            file: "circuits/fetcher",
+            template: "ExtractValue",
+            params: [12, 1, 1, 1],
+        });
+
+        let [input, keyUnicode, output] = readInputFile("value_string.json", "k");
+
+
+        console.log("#constraints:", await circuit.getConstraintCount());
+
+        await circuit.expectPass({
+            data: input, key: keyUnicode,
+        }, {
+            value: output,
+        });
+    });
+
+    it("two_keys: {\"key1\": \"abc\", \"key2\": \"def\" }", async () => {
+        circuit = await circomkit.WitnessTester(`Extract`, {
+            file: "circuits/fetcher",
+            template: "ExtractValue",
+            params: [40, 1, 4, 3],
+        });
+        console.log("#constraints:", await circuit.getConstraintCount());
+
+        let [input1, keyUnicode1, output1] = readInputFile("two_keys.json", "key1");
+
+        await circuit.expectPass({ data: input1, key: keyUnicode1 }, { value: output1 });
+
+
+        let [input2, keyUnicode2, output2] = readInputFile("two_keys.json", "key2");
+
+        await circuit.expectPass({ data: input2, key: keyUnicode2 }, { value: output2 });
+
+    });
+});
