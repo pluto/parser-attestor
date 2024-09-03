cc479381da2e9ca8c0298671b274ade977eef2d8
Colin Roberts <colin@autoparallel.xyz>
2024-09-03 15:04:35 -0600

    feat: Rust CLI improvements and HTTP lockfile example (#72)
    * feat: combined CLI
    
    * refactor and add example http lock
    
    * http lock example
    
    * include `search/witness.json`
    
    * fix tests
    
    * clean up basic lock file

---
+++ src/http_lock.rs
@@ -0,0 +1,34 @@
+use super::*;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct HttpData {
+    request: Request,
+    response: Response,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Request {
+    method: String,
+    target: String,
+    version: String,
+    headers: Vec<(String, String)>,
+    #[serde(rename = "Host")]
+    host: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Response {
+    version: String,
+    status: String,
+    headers: Vec<(String, serde_json::Value)>,
+}
+
+// TODO: This needs to codegen a circuit now.
+pub fn http_lock(args: HttpLockArgs) -> Result<(), Box<dyn Error>> {
+    let data = std::fs::read(&args.lockfile)?;
+    let http_data: HttpData = serde_json::from_slice(&data)?;
+
+    dbg!(http_data);
+
+    Ok(())
+}
