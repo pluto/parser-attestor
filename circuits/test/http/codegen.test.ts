620cf0f206c452800c7fad4f2db59aac9cbbc696
Colin Roberts <colin@autoparallel.xyz>
2024-09-04 16:05:25 -0600

    Update codegen.test.ts

--- circuits/test/http/codegen.test.ts
+++ circuits/test/http/codegen.test.ts
@@ -57,7 +57,7 @@ function executeCodegen(inputFilename: string, outputFilename: string) {
 describe("HTTP :: Codegen", async () => {
     let circuit: WitnessTester<["data", "beginning", "middle", "final", "header1", "value1", "header2", "value2"], []>;
 
-    it("get_request:", async () => {
+    it("(valid) get_request:", async () => {
         let lockfile = "test.lock";
         let inputfile = "get_request.http";
 
@@ -76,7 +76,6 @@ describe("HTTP :: Codegen", async () => {
             console.log("header: ", header[0]);
             console.log("value: ", header[1]);
         });
-        console.log("params: ", params);
 
 
         circuit = await circomkit.WitnessTester(`Extract`, {
@@ -97,6 +96,44 @@ describe("HTTP :: Codegen", async () => {
             header2: toByte(lockData.request.headers[1][0]),
             value2: toByte(lockData.request.headers[1][1])
         },
-            {});
+            {}
+        );
+    });
+
+    it("(invalid) get_request:", async () => {
+        let lockfile = "test.lock";
+        let inputfile = "get_request.http";
+
+        // generate extractor circuit using codegen
+        await executeCodegen(`${lockfile}.json`, lockfile);
+
+        const lockData = await readLockFile(`${lockfile}.json`);
+
+        const input = await readHTTPInputFile(`${inputfile}`).input
+
+        const params = [input.length, lockData.request.method.length, lockData.request.target.length, lockData.request.version.length];
+        lockData.request.headers.forEach(header => {
+            params.push(header[0].length);  // Header name length
+            params.push(header[1].length);  // Header value length
+        });
+
+
+        circuit = await circomkit.WitnessTester(`Extract`, {
+            file: `circuits/main/${lockfile}`,
+            template: "LockHTTP",
+            params: params,
+        });
+        console.log("#constraints:", await circuit.getConstraintCount());
+
+        await circuit.expectFail({
+            data: input.slice(0),
+            beginning: toByte(lockData.request.method),
+            middle: toByte(lockData.request.target),
+            final: toByte(lockData.request.version),
+            header1: toByte(lockData.request.headers[0][0]),
+            value1: toByte("/aip"),
+            header2: toByte(lockData.request.headers[1][0]),
+            value2: toByte(lockData.request.headers[1][1])
+        });
     });
 });
