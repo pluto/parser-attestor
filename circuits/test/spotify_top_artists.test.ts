c683f3a80ed3ffb3339b832e735fa7a4a048f181
lonerapier <lonerapier@proton.me>
2024-09-20 16:04:03 +0530

    save work till now, start on integrated codegen

--- circuits/test/spotify_top_artists.test.ts
+++ circuits/test/spotify_top_artists.test.ts
@@ -20,7 +20,7 @@ describe("spotify top artists", async () => {
 
         let index_0 = 0;
 
-        let [input, key, output] = readJSONInputFile(
+        let [inputJson, key, output] = readJSONInputFile(
             `${jsonFilename}.json`,
             [
                 "data",
@@ -35,30 +35,29 @@ describe("spotify top artists", async () => {
             ]
         );
 
-        json_circuit = await circomkit.WitnessTester(`Extract`, {
-            file: `main/json_${jsonFilename}_test`,
-            template: "ExtractStringValue",
-            params: [input.length, 9, 4, 0, 2, 1, 7, 2, 10, 3, 5, 4, index_0, 5, 4, 6, 7, 7, 4, 8, 12],
-        });
-        console.log("#constraints:", await json_circuit.getConstraintCount());
-
-        await json_circuit.expectPass({ data: input, key1: key[0], key2: key[1], key3: key[2], key4: key[3], key5: key[4], key7: key[6], key8: key[7], key9: key[8] }, { value: output });
-
-        // const lockData = readLockFile<Response>(`${httpLockfile}.json`);
-        // console.log("lockData: ", JSON.stringify(lockData));
-
-        // const http = readHTTPInputFile(`${httpInputFile}`);
-        // const input = http.input;
-
-        // const headers = getHttpHeaders(lockData);
-
-        // const params = [input.length, http.bodyBytes.length, lockData.version.length, lockData.status.length, lockData.message.length];
-        // headers.forEach(header => {
-        //     params.push(header[0].length);
-        //     params.push(header[1].length);
+        // json_circuit = await circomkit.WitnessTester(`Extract`, {
+        //     file: `main/json_${jsonFilename}_test`,
+        //     template: "ExtractStringValue",
+        //     params: [input.length, 9, 4, 0, 2, 1, 7, 2, 10, 3, 5, 4, index_0, 5, 4, 6, 7, 7, 4, 8, 12],
         // });
+        // console.log("#constraints:", await json_circuit.getConstraintCount());
 
-        // console.log("starting witness gen");
+        // await json_circuit.expectPass({ data: input, key1: key[0], key2: key[1], key3: key[2], key4: key[3], key5: key[4], key7: key[6], key8: key[7], key9: key[8] }, { value: output });
+
+        const lockData = readLockFile<Response>(`${httpLockfile}.json`);
+        console.log("lockData: ", JSON.stringify(lockData));
+
+        const http = readHTTPInputFile(`${httpInputFile}`);
+        const inputHttp = http.input;
+
+        const headers = getHttpHeaders(lockData);
+
+        const params = [inputHttp.length, http.bodyBytes.length, lockData.version.length, lockData.status.length, lockData.message.length];
+        headers.forEach(header => {
+            params.push(header[0].length);
+            params.push(header[1].length);
+        });
+
         // http_circuit = await circomkit.WitnessTester(`Extract`, {
         //     file: `main/http_${httpCircuitName}`,
         //     template: "LockHTTPResponse",
@@ -66,20 +65,18 @@ describe("spotify top artists", async () => {
         // });
         // console.log("#constraints:", await http_circuit.getConstraintCount());
 
-        // // match circuit output to original JSON value
-        // const circuitInput: any = {
-        //     data: input,
-        //     version: toByte(lockData.version),
-        //     status: toByte(lockData.status),
-        //     message: toByte(lockData.message),
-        // };
+        // match circuit output to original JSON value
+        const circuitInput: any = {
+            data: inputHttp,
+            version: toByte(lockData.version),
+            status: toByte(lockData.status),
+            message: toByte(lockData.message),
+        };
 
-        // headers.forEach((header, index) => {
-        //     circuitInput[`header${index + 1}`] = toByte(header[0]);
-        //     circuitInput[`value${index + 1}`] = toByte(header[1]);
-        // });
-
-        // console.log("circuitInput", circuitInput)
+        headers.forEach((header, index) => {
+            circuitInput[`header${index + 1}`] = toByte(header[0]);
+            circuitInput[`value${index + 1}`] = toByte(header[1]);
+        });
 
         // await http_circuit.expectPass(circuitInput, { body: http.bodyBytes });
     });
