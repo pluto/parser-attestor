0f17e7f4ae438a226aed328c0c9f9f90cc2e6e28
lonerapier <lonerapier@proton.me>
2024-09-21 17:52:46 +0530

    test: add separate test for correctness

--- circuits/test/spotify_top_artists.test.ts
+++ circuits/test/spotify_top_artists.test.ts
@@ -2,10 +2,37 @@ import { circomkit, WitnessTester, toByte, readJSONInputFile } from "./common";
 import { readLockFile, readHTTPInputFile, getHeaders as getHttpHeaders, Response } from "./common/http";
 import { executeCodegen as httpLockfileCodegen } from "./http/codegen.test";
 import { executeCodegen as jsonLockfileCodegen } from "./json/extractor/extractor.test";
+import { join } from "path";
+import { spawn } from "child_process";
+
+async function extendedLockfileCodegen(circuitName: string, inputFileName: string, lockfileName: string) {
+    return new Promise((resolve, reject) => {
+        const inputFilePath = join(__dirname, "..", "..", "..", "examples", "http", inputFileName);
+        const lockfilePath = join(__dirname, "..", "..", "..", "examples", "http", "lockfile", lockfileName);
+
+        const codegen = spawn("cargo", ["run", "codegen", "extended", "--circuit-name", circuitName, "--input-file", inputFilePath, "--lockfile", lockfilePath]);
+
+        codegen.stdout.on('data', (data) => {
+            console.log(`stdout: ${data}`);
+        });
+
+        codegen.stderr.on('data', (data) => {
+            console.error(`stderr: ${data}`);
+        });
+
+        codegen.on('close', (code) => {
+            if (code === 0) {
+                resolve(`child process exited with code ${code}`); // Resolve the promise if the process exits successfully
+            } else {
+                reject(new Error(`Process exited with code ${code}`)); // Reject if there's an error
+            }
+        });
+    })
+}
 
 describe("spotify top artists", async () => {
     let http_circuit: WitnessTester<["data", "version", "status", "message", "header1", "value1", "header2", "value2"], ["body"]>;
-    let json_circuit: WitnessTester<["data", "key1", "key2", "key3", "key4", "key5", "key7", "key8", "key9"], ["value"]>;
+    let json_circuit: WitnessTester<["data", "key1", "key2", "key4", "key5"], ["value"]>;
 
     it("POST response body extraction", async () => {
         let httpLockfile = "spotify.lock"
@@ -17,32 +44,6 @@ describe("spotify top artists", async () => {
         let jsonFilename = "spotify";
 
         await jsonLockfileCodegen(`${jsonFilename}_test`, `${jsonFilename}.json`, `${jsonFilename}.json`);
-
-        let index_0 = 0;
-
-        let [inputJson, key, output] = readJSONInputFile(
-            `${jsonFilename}.json`,
-            [
-                "data",
-                "me",
-                "profile",
-                "topArtists",
-                "items",
-                index_0,
-                "data",
-                "profile",
-                "name"
-            ]
-        );
-
-        // json_circuit = await circomkit.WitnessTester(`Extract`, {
-        //     file: `main/json_${jsonFilename}_test`,
-        //     template: "ExtractStringValue",
-        //     params: [input.length, 9, 4, 0, 2, 1, 7, 2, 10, 3, 5, 4, index_0, 5, 4, 6, 7, 7, 4, 8, 12],
-        // });
-        // console.log("#constraints:", await json_circuit.getConstraintCount());
-
-        // await json_circuit.expectPass({ data: input, key1: key[0], key2: key[1], key3: key[2], key4: key[3], key5: key[4], key7: key[6], key8: key[7], key9: key[8] }, { value: output });
 
         const lockData = readLockFile<Response>(`${httpLockfile}.json`);
         console.log("lockData: ", JSON.stringify(lockData));
@@ -58,12 +59,12 @@ describe("spotify top artists", async () => {
             params.push(header[1].length);
         });
 
-        // http_circuit = await circomkit.WitnessTester(`Extract`, {
-        //     file: `main/http_${httpCircuitName}`,
-        //     template: "LockHTTPResponse",
-        //     params: params,
-        // });
-        // console.log("#constraints:", await http_circuit.getConstraintCount());
+        http_circuit = await circomkit.WitnessTester(`Extract`, {
+            file: `main/http_${httpCircuitName}`,
+            template: "LockHTTPResponse",
+            params: params,
+        });
+        console.log("#constraints:", await http_circuit.getConstraintCount());
 
         // match circuit output to original JSON value
         const circuitInput: any = {
@@ -78,6 +79,28 @@ describe("spotify top artists", async () => {
             circuitInput[`value${index + 1}`] = toByte(header[1]);
         });
 
-        // await http_circuit.expectPass(circuitInput, { body: http.bodyBytes });
+        await http_circuit.expectPass(circuitInput, { body: http.bodyBytes });
+
+        let index_0 = 0;
+
+        let [inputJson, key, output] = readJSONInputFile(
+            `${jsonFilename}.json`,
+            [
+                "data",
+                "items",
+                index_0,
+                "profile",
+                "name"
+            ]
+        );
+
+        json_circuit = await circomkit.WitnessTester(`Extract`, {
+            file: `main/json_${jsonFilename}_test`,
+            template: "ExtractStringValue",
+            params: [inputJson.length, 5, 4, 0, 5, 1, index_0, 2, 7, 3, 4, 4, 12],
+        });
+        console.log("#constraints:", await json_circuit.getConstraintCount());
+
+        await json_circuit.expectPass({ data: inputJson, key1: key[0], key2: key[1], key4: key[3], key5: key[4] }, { value: output });
     });
 })
